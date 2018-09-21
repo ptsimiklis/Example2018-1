944736237f46890717a354e712cdf98cdfd5dd41
Peter <peter.tsimiklis@gmail.com>
2018-09-21 14:52:29 -0400

    added ring tests

--- src/Example2018.jl
+++ src/Example2018.jl
@@ -34,9 +34,15 @@ function evaluate(p::Polynomial, x)
     sum(c * x^(i-1) for (i,c) in enumerate(p.coeffs))
 end
 
+function Base. ==(p::Polynomial, q::Polynomial)
+    d = p - q
+    # all(c -> c==0, d.coeffs)
+    all(d.coeffs .== 0)
+end
+
 function Base. +(p::Polynomial, q::Polynomial)
     l = max(length(p.coeffs), length(q.coeffs))
-    rcoeffs = zeros(l)
+    rcoeffs = zeros(eltype(p.coeffs), l)
     for (i,c) in enumerate(p.coeffs)
         rcoeffs[i] += c
     end
@@ -51,11 +57,17 @@ function Base. *(a, p::Polynomial)
 end
 Base. *(p::Polynomial, a) = a*p
 
+function Base. -(p::Polynomial)
+    (-1) * p
+end
+
+function Base. -(p::Polynomial, q::Polynomial)
+    p + (-q)
+end
+
 export deriv
 function deriv(p::Polynomial)
     Polynomial([(i-1) * c for (i,c) in enumerate(p.coeffs)][2:end])
 end
-
-
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,25 @@
+using Test
 using Example2018
 
-@show Polynomial([1,2,3])
-@show evaluate(Polynomial([1,2,3]), 2)
-@show Polynomial([1,2,3]) + Polynomial([4,5])
-@show 3 * Polynomial([1,2,4,42])
-@show deriv(Polynomial([1,1,1,1,1]))
-@show deriv(Polynomial([0,0,1,0,0]))
+
+# polynomials are a ring
+# 1. addition is associative
+# 2. additive identity
+# 3. addotove inverse
+# 4. addition commutes
+# 5. multiplication is ...
+
+@testset "Polynomials form a ring" begin
+
+   z = Polynomial([0])
+   for n in 1:100
+      deg = rand(0:100)
+      p = Polynomial(rand(-10000000:1000000, deg+1))
+
+      @test p + z == p
+      @test z + p == p
+      @test p + (-p) == z
+
+   end
+
+end
